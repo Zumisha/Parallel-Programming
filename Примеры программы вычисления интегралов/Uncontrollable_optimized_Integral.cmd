0095ae6b152b226badddb7c44da272c2bda45803
zumisha <zumisha@yandex.ru>
2019-04-04 02:06:11 +0300

    Переписана. Стала эффективнее и проще для понимания.

--- Примеры программы вычисления интегралов/Uncontrollable_optimized_Integral.cmd
+++
@@ -1,3 +0,0 @@
-@ECHO off
-..\fptl.exe --source-file Uncontrollable_optimized_Integral.txt --num-cores 8
-pause
--- Примеры программы вычисления интегралов/Uncontrollable_optimized_Integral_2.cmd
+++
@@ -1,3 +0,0 @@
-@ECHO off
-..\fptl.exe --source-file Uncontrollable_optimized_Integral_2.txt --num-cores 8
-pause
